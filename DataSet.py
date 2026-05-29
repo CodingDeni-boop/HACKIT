c5d6029ee8d425e2ecd81d9d8c1eade70eb0c5ed
CodingDeni-boop <dbugatti@ethz.ch>
2026-05-30 00:56:18 +0200

    aqegkjqbefgn

--- DataSet.py
+++ DataSet.py
@@ -1,3 +1,16 @@
 from vinted import Vinted
+import torch
 
-vinted = Vinted()
+
+vinted = Vinted(domain = "com")
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+    print("WARNING!!!   no gpu acceleration was found")
+
+print(f"running on: {device}")
+
