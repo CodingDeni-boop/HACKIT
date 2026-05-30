40f8e97c81263d1cfd88d77d53c7588ded6a6167
CodingDeni-boop <dbugatti@ethz.ch>
2026-05-30 03:17:57 +0200

    vuy

--- DataSet.py
+++ DataSet.py
@@ -1,14 +1,23 @@
 from wallapy import check_wallapop
+from torch.utils.data.dataset import Dataset
+
+NAME = "shoe"
+SIZE = 10
 
 results = check_wallapop(
-    product_name="giacca di jeans",   # <-- il primo argomento è product_name, non keywords
-    max_price=40,
-    max_total_items=20,
-    deep_search=False,                # più veloce: per le immagini non serve
+    product_name=NAME,
+    max_total_items=SIZE
 )
 
-item = results[0]
+print(results)
+"""
 print(item["title"], item["price"], item["currency"])
 print(item["link"])              # link all'annuncio
 print(item["main_image"])        # URL della foto principale
 print(item["all_images"])        # tutte le foto
+"""
+class ClassDataset():
+    pass
+
+for item in results:
+    print(item["main_image"])
